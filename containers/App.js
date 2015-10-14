79a6714fa6e66994025ca1035cbf6cda5a929866
roylee0704 <roylee0704@gmail.com>
2015-10-14 17:42:31 +0800

    setup for react :

---
+++ containers/App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends Component {
+
+    render() {
+        return (<div> Hello From React-Boilerplate </div>);
+    }
+}
