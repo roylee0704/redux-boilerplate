6dc3da441da50968a84369051425bd05c5c16662
roylee0704 <roylee0704@gmail.com>
2015-11-23 12:09:16 +0800

    updated dir(s) and boilerplate(s)

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import 'babel-core/polyfill';
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+
+import App from './containers/App';
+import rootReducer from './reducers/index';
+import 'stylesheets/base';
+
+
+const loggerMiddleware = createLogger();
+const createStoreWithMiddleware = applyMiddleware(loggerMiddleware, thunkMiddleware)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
+
+injectTapEventPlugin();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
