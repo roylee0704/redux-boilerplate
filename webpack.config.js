381f0f075c2b5e2f1cffe6b7094ca134d0a75722
roylee0704 <roylee0704@gmail.com>
2015-11-23 11:41:34 +0800

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,75 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './src')
+];
+
+const config = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './index'
+    path.resolve(__dirname, './src/index')
   ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      }
+    ]
+  },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, './dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('bundle.css')
   ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      exclude: /node_modules/,
-      include: __dirname
-    }]
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.sass'],
+    modulesDirectories: ['src', 'node_modules']
   }
 };
-
 
 // When inside Redux repo, prefer src to compiled version.
 // You can safely delete these lines in your project.
 var reduxSrc = path.join(__dirname, '..', '..', 'src');
 var reduxNodeModules = path.join(__dirname, '..', '..', 'node_modules');
 var fs = require('fs');
+
 if (fs.existsSync(reduxSrc) && fs.existsSync(reduxNodeModules)) {
   // Resolve Redux to source
-  module.exports.resolve = { alias: { 'redux': reduxSrc } };
+  config.resolve.alias = {
+    'redux': reduxSrc
+  };
+
   // Compile Redux from source
-  module.exports.module.loaders.push({
+  config.module.loaders.push({
     test: /\.js$/,
     loaders: ['babel'],
     include: reduxSrc
   });
 }
+
+module.exports = config;
