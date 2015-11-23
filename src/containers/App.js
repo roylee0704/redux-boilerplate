e992b4e3922a03cbbbf39ff86d66fc40cd2fd79c
roylee0704 <roylee0704@gmail.com>
2015-11-23 12:15:31 +0800

    updated boilerplate container/app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,18 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
 
-export default class App extends Component {
-
-    render() {
-        return (<div> Hello From React-Boilerplate </div>);
-    }
+class App extends Component {
+  render() {
+    return (<div> Hello From React-Boilerplate </div>);
+  }
 }
+
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return state;
+};
+
+export default connect(mapStateToProps)(App);
